1054dd9308aca3587c78712ae3763dda1cdacce0
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2019-06-12 12:55:38 +0200

    Mise au point d'une fonction de renvoie de l'UTC sous forme de timestamp, et développement d'une bibliothéque de fonctions utiles

---
+++ src/bibliotheque_de_fonctions_utiles.c
@@ -0,0 +1,26 @@
+#include <time.h>
+#include "bibliotheque_de_fonctions_utiles.h"
+
+//Cette fonction renvoie le temps actuel (heure et date actuelles) UTC (Coordinated Universal Time), donc le temps universel coordonné
+time_t retour_du_temps_utc_sous_forme_de_timestamp()
+{
+
+	//Déclaration de la variable temps_utc_sous_forme_de_struct_tm: Cette variable contiendra le temps UTC (heure et date) sous forme de struct tm
+	struct tm *temps_utc_sous_forme_de_struct_tm;
+
+	//Déclaration de la variable temps_utc_sous_forme_de_timestamp: Cette variable contiendra le temps UTC (heure et date) sous forme de timestamp
+	time_t temps_utc_sous_forme_de_timestamp;
+
+	//Déclaration et initialisation de la variable temps_actuel_sous_forme_de_timestamp: Cette variable contient le temps actuel (heure et date) sous forme de timestamp
+	time_t temps_actuel_sous_forme_de_timestamp = time(NULL);
+
+	//
+	temps_utc_sous_forme_de_struct_tm = gmtime(&temps_actuel_sous_forme_de_timestamp);
+
+	//
+	temps_utc_sous_forme_de_timestamp = timegm(temps_utc_sous_forme_de_struct_tm);
+
+	//Le temps UTC, sous forme de timestamp, est retourné
+	return temps_utc_sous_forme_de_timestamp;
+
+}
---
+++ src/bibliotheque_de_fonctions_utiles.h
@@ -0,0 +1,9 @@
+#ifndef BIBLIOTHEQUE_DE_FONCTIONS_UTILES
+#define BIBLIOTHEQUE_DE_FONCTIONS_UTILES
+
+#include <time.h>
+
+//Cette fonction renvoie le temps actuel (heure et date actuelles) UTC (Coordinated Universal Time), donc le temps universel coordonné
+time_t retour_du_temps_utc_sous_forme_de_timestamp();
+
+#endif
