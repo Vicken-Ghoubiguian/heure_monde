98da75dd15597e85424a3f422c58dcd8f6e54a04
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2019-06-13 17:07:27 +0200

    Mise en commentaire des tests sur la fonction de retour d'un horaire sur un fuseau particulier pour nous focaliser sur la migration de l'affichage de l'horloge vers l'UTC

--- test/main.c
+++ test/main.c
@@ -8,28 +8,28 @@ int main()
 	affichage_de_l_horloge(time(NULL));
 
 	//
-	char* premier_test = retour_de_l_heure_et_de_la_date_pour_une_ville_determinee_et_connue(time(NULL), "Seoul");
+	//char* premier_test = retour_de_l_heure_et_de_la_date_pour_une_ville_determinee_et_connue(time(NULL), "Seoul");
 
 	//
-	printf("%s\n", premier_test);
+	//printf("%s\n", premier_test);
 
 	//
-	char* second_test = retour_de_l_heure_et_de_la_date_pour_une_ville_determinee_et_connue(time(NULL), "Addis-Abeba");
+	//char* second_test = retour_de_l_heure_et_de_la_date_pour_une_ville_determinee_et_connue(time(NULL), "Addis-Abeba");
 
 	//
-	printf("%s\n", second_test);
+	//printf("%s\n", second_test);
 
 	//
-	char* troisieme_test = retour_de_l_heure_et_de_la_date_pour_une_ville_determinee_et_connue(time(NULL), "Hong Kong");
+	//char* troisieme_test = retour_de_l_heure_et_de_la_date_pour_une_ville_determinee_et_connue(time(NULL), "Hong Kong");
 
 	//
-	printf("%s\n", troisieme_test);
+	//printf("%s\n", troisieme_test);
 
 	//
-	char* quatrieme_test = retour_de_l_heure_et_de_la_date_pour_une_ville_determinee_et_connue(time(NULL), "Enfin le dernier");
+	//char* quatrieme_test = retour_de_l_heure_et_de_la_date_pour_une_ville_determinee_et_connue(time(NULL), "Enfin le dernier");
 
 	//
-	printf("%s\n", quatrieme_test);
+	//printf("%s\n", quatrieme_test);
 
 	//
 	return 0;
