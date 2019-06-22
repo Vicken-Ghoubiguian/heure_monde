de33ba669a98c5128031c91d44f0ec9381402252
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2019-06-22 22:20:55 +0200

    Suppréssion des fonctions inutiles

--- src/application_des_decalages_horaires.h
+++ src/application_des_decalages_horaires.h
@@ -3,24 +3,6 @@
 
 #include <time.h>
 
-//Cette fonction permet de déterminer si il faut (ou non) en fonction du changement d'heure ajouter une heure (ou non) pour le calcul des fuseaux horaires de Pekin, Tokyo, Seoul, Moscou, et Vladivostok
-int application_de_l_heure_d_ete_pour_les_fuseaux_sans_changements(time_t date);
-
-//Cette fonction permet de calculer le decallage horaire d'Auckland (Nouvelle-Zelande) avec l'heure française
-int calcul_du_decalage_avec_auckland(time_t aujourdhui);
-
-//Cette fonction permet de calculer le decallage horaire de l'Australie avec l'heure française
-int calcul_du_decalage_avec_l_australie(time_t aujourdhui);
-
-//Cette fonction permet de calculer le decallage horaire de Santiago du Chili (Chili) avec l'heure française
-int calcul_du_decalage_avec_le_chili(time_t aujourdhui);
-
-//Cette fonction permet de calculer le decallage horaire de Los Angeles et de New York City (USA) avec l'heure française
-int calcul_du_decalage_avec_l_amerique_du_nord(time_t aujourdhui);
-
-//Cette fonction permet de calculer le decallage horaire de Londres avec l'heure française
-int calcul_du_decallage_avec_la_grande_bretagne(time_t aujourdhui);
-
 //Cette fonction permet d'appliquer le changement d'heure pour l'Europe continentale
 int application_du_changement_d_heure_pour_l_europe_continentale(time_t temps_courant);
 
