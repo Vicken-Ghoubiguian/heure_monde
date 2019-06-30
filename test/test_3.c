1919b58d1088e9760b7f329db2f1ef2ccbb56d9a
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2019-06-30 21:59:44 +0200

    Traitemment et vérification de l'année voulue dans le test 3

--- test/test_3.c
+++ test/test_3.c
@@ -18,39 +18,56 @@ int main(int argc, char* argv[])
 	//Déclaration de la variable timestamp_courant_avec_annee_voulue qui représente le timestamp sur lequel on afféctera l'année voulue
 	time_t timestamp_courant_avec_annee_voulue;
 
+	//Déclaration de la variable annee_voulue qui contient l'année passée en argument du programme
+	int annee_voulue;
+
 	//Si le nombre d'arguments passé en paramétre est égal à 2, alors...
 	if(argc == 2)
 	{
 
-		//Déclaration de la variable annee_voulue qui contient l'année passée en argument du programme, convertie sous forme de int grâce à la fonction atoi
-		int annee_voulue = atoi(argv[1]);
+		//Afféctation de la valeur en argument du programme représentant l'année en cours dans la variable annee_voulue, mais d'abord convertie sous forme de int grâce à la fonction atoi
+		annee_voulue = atoi(argv[1]);
 
-		//Déclaration des variables de changement d'heure heure d'été <=> heure d'hiver, pour toutes les zones géographique l'appliquant, sous forme de timestamp
-		time_t horaire_de_passage_a_l_heure_d_ete_pour_les_samoa_sous_forme_de_timestamp;
-		time_t horaire_de_passage_a_l_heure_d_hiver_pour_les_samoa_sous_forme_de_timestamp;
+		//Si l'année voulue est strictement supérieur à 0, et donc si elle est valide par conséquent, alors...
+		if(0 < annee_voulue)
+		{
 
-		//Déclaration des variables de changement d'heure heure d'été <=> heure d'hiver, pour toutes les zones géographique l'appliquant, sous forme de char*
-		char* horaire_de_passage_a_l_heure_d_ete_pour_les_samoa_sous_forme_de_chaine_de_caracteres;
-		char* horaire_de_passage_a_l_heure_d_hiver_pour_les_samoa_sous_forme_de_chaine_de_caracteres;
+			//Déclaration des variables de changement d'heure heure d'été <=> heure d'hiver, pour toutes les zones géographique l'appliquant, sous forme de timestamp
+			time_t horaire_de_passage_a_l_heure_d_ete_pour_les_samoa_sous_forme_de_timestamp;
+			time_t horaire_de_passage_a_l_heure_d_hiver_pour_les_samoa_sous_forme_de_timestamp;
 
-		//Affectation de la valeur du timestamp du temps courant à l'année voulue
-		timestamp_courant_avec_annee_voulue = fonction_de_precision_de_l_annee_en_cours(timestamp_courant, annee_voulue);
+			//Déclaration des variables de changement d'heure heure d'été <=> heure d'hiver, pour toutes les zones géographique l'appliquant, sous forme de char*
+			char* horaire_de_passage_a_l_heure_d_ete_pour_les_samoa_sous_forme_de_chaine_de_caracteres;
+			char* horaire_de_passage_a_l_heure_d_hiver_pour_les_samoa_sous_forme_de_chaine_de_caracteres;
 
-		//
+			//Affectation de la valeur du timestamp du temps courant à l'année voulue
+			timestamp_courant_avec_annee_voulue = fonction_de_precision_de_l_annee_en_cours(timestamp_courant, annee_voulue);
 
-		/* État indépendant des Samoa */
-		horaire_de_passage_a_l_heure_d_ete_pour_les_samoa_sous_forme_de_timestamp = date_du_dernier_dimanche_de_septembre(timestamp_courant_avec_annee_voulue);
-		horaire_de_passage_a_l_heure_d_hiver_pour_les_samoa_sous_forme_de_timestamp = date_du_premier_dimanche_d_avril(timestamp_courant_avec_annee_voulue, 3);
+			//
 
-		//
+			/* État indépendant des Samoa */
+			horaire_de_passage_a_l_heure_d_ete_pour_les_samoa_sous_forme_de_timestamp = date_du_dernier_dimanche_de_septembre(timestamp_courant_avec_annee_voulue);
+			horaire_de_passage_a_l_heure_d_hiver_pour_les_samoa_sous_forme_de_timestamp = date_du_premier_dimanche_d_avril(timestamp_courant_avec_annee_voulue, 3);
 
-		/* État indépendant des Samoa */
-		horaire_de_passage_a_l_heure_d_ete_pour_les_samoa_sous_forme_de_chaine_de_caracteres = calcul_et_renvoie_de_l_heure_et_de_la_date_en_fonction_du_timestamp_passe_en_parametre(horaire_de_passage_a_l_heure_d_ete_pour_les_samoa_sous_forme_de_timestamp);
-		horaire_de_passage_a_l_heure_d_hiver_pour_les_samoa_sous_forme_de_chaine_de_caracteres = calcul_et_renvoie_de_l_heure_et_de_la_date_en_fonction_du_timestamp_passe_en_parametre(horaire_de_passage_a_l_heure_d_hiver_pour_les_samoa_sous_forme_de_timestamp);
+			//
 
-		//Affichage de tous les changements d'heure heure d'été <=> heure d'hiver pour l'année annee_voulue
-		printf("Passage à l'heure d'été en %d pour l'État indépendant des Samoa: %s\n", annee_voulue, horaire_de_passage_a_l_heure_d_ete_pour_les_samoa_sous_forme_de_chaine_de_caracteres);
-		printf("Passage à l'heure d'hiver en %d pour l'État indépendant des Samoa: %s\n", annee_voulue, horaire_de_passage_a_l_heure_d_hiver_pour_les_samoa_sous_forme_de_chaine_de_caracteres);
+			/* État indépendant des Samoa */
+			horaire_de_passage_a_l_heure_d_ete_pour_les_samoa_sous_forme_de_chaine_de_caracteres = calcul_et_renvoie_de_l_heure_et_de_la_date_en_fonction_du_timestamp_passe_en_parametre(horaire_de_passage_a_l_heure_d_ete_pour_les_samoa_sous_forme_de_timestamp);
+			horaire_de_passage_a_l_heure_d_hiver_pour_les_samoa_sous_forme_de_chaine_de_caracteres = calcul_et_renvoie_de_l_heure_et_de_la_date_en_fonction_du_timestamp_passe_en_parametre(horaire_de_passage_a_l_heure_d_hiver_pour_les_samoa_sous_forme_de_timestamp);
+
+			//Affichage de tous les changements d'heure heure d'été <=> heure d'hiver pour l'année annee_voulue
+			printf("Passage à l'heure d'été en %d pour l'État indépendant des Samoa: %s\n", annee_voulue, horaire_de_passage_a_l_heure_d_ete_pour_les_samoa_sous_forme_de_chaine_de_caracteres);
+			printf("Passage à l'heure d'hiver en %d pour l'État indépendant des Samoa: %s\n", annee_voulue, horaire_de_passage_a_l_heure_d_hiver_pour_les_samoa_sous_forme_de_chaine_de_caracteres);
+
+		}
+		//Sinon...
+		else
+		{
+
+			//Affichage d'un message d'avertissement en cas où l'année passée en argument du programme n'est pas valide
+			printf("L'année passée en paramétre du programme éxecutable n'est pas valide.\n");
+
+		}
 
 	}
 	//Sinon...
