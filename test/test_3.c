ba8ba0996ff7b79c177690809be10936f705e3cf
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2019-07-01 20:30:34 +0200

    Ajout du changement d'heure heure d'été <=> heure d'hiver dans le test 3 pour l'Amérique du Nord

--- test/test_3.c
+++ test/test_3.c
@@ -37,12 +37,16 @@ int main(int argc, char* argv[])
 			time_t horaire_de_passage_a_l_heure_d_hiver_pour_les_samoa_sous_forme_de_timestamp;
 			time_t horaire_de_passage_a_l_heure_d_ete_pour_l_europe_continentale_sous_forme_de_timestamp;
 			time_t horaire_de_passage_a_l_heure_d_hiver_pour_l_europe_continentale_sous_forme_de_timestamp;
+			time_t horaire_de_passage_a_l_heure_d_ete_pour_l_amerique_du_nord_sous_forme_de_timestamp;
+			time_t horaire_de_passage_a_l_heure_d_hiver_pour_l_amerique_du_nord_sous_forme_de_timestamp;
 
-			//Déclaration des variables de changement d'heure heure d'été <=> heure d'hiver, pour toutes les zones géographique l'appliquant, sous forme de char*
+			//Déclaration des variables de changement d'heure heure d'été <=> heure d'hiver, pour toutes les zones géographiques l'appliquant, sous forme de char*
 			char* horaire_de_passage_a_l_heure_d_ete_pour_les_samoa_sous_forme_de_chaine_de_caracteres;
 			char* horaire_de_passage_a_l_heure_d_hiver_pour_les_samoa_sous_forme_de_chaine_de_caracteres;
 			char* horaire_de_passage_a_l_heure_d_ete_pour_l_europe_continentale_sous_forme_de_chaine_de_caracteres;
 			char* horaire_de_passage_a_l_heure_d_hiver_pour_l_europe_continentale_sous_forme_de_chaine_de_caracteres;
+			char* horaire_de_passage_a_l_heure_d_ete_pour_l_amerique_du_nord_sous_forme_de_chaine_de_caracteres;
+			char* horaire_de_passage_a_l_heure_d_hiver_pour_l_amerique_du_nord_sous_forme_de_chaine_de_caracteres;
 
 			//Affectation de la valeur du timestamp du temps courant à l'année voulue
 			timestamp_courant_avec_annee_voulue = fonction_de_precision_de_l_annee_en_cours(timestamp_courant, annee_voulue);
@@ -57,6 +61,10 @@ int main(int argc, char* argv[])
 			horaire_de_passage_a_l_heure_d_ete_pour_l_europe_continentale_sous_forme_de_timestamp = date_du_dernier_dimanche_de_mars(timestamp_courant_avec_annee_voulue, 1);
 			horaire_de_passage_a_l_heure_d_hiver_pour_l_europe_continentale_sous_forme_de_timestamp = date_du_dernier_dimanche_d_octobre(timestamp_courant_avec_annee_voulue, 1);
 
+			/* Amérique du Nord */
+			horaire_de_passage_a_l_heure_d_ete_pour_l_amerique_du_nord_sous_forme_de_timestamp = date_du_deuxieme_dimanche_de_mars(timestamp_courant_avec_annee_voulue);
+			horaire_de_passage_a_l_heure_d_hiver_pour_l_amerique_du_nord_sous_forme_de_timestamp = date_du_premier_dimanche_de_novembre(timestamp_courant_avec_annee_voulue);
+
 			//
 
 			/* État indépendant des Samoa */
@@ -66,6 +74,10 @@ int main(int argc, char* argv[])
 			/* Europe continentale */
 			horaire_de_passage_a_l_heure_d_ete_pour_l_europe_continentale_sous_forme_de_chaine_de_caracteres = calcul_et_renvoie_de_l_heure_et_de_la_date_en_fonction_du_timestamp_passe_en_parametre(horaire_de_passage_a_l_heure_d_ete_pour_l_europe_continentale_sous_forme_de_timestamp);
 			horaire_de_passage_a_l_heure_d_hiver_pour_l_europe_continentale_sous_forme_de_chaine_de_caracteres = calcul_et_renvoie_de_l_heure_et_de_la_date_en_fonction_du_timestamp_passe_en_parametre(horaire_de_passage_a_l_heure_d_hiver_pour_l_europe_continentale_sous_forme_de_timestamp);
+
+			/* Amérique du Nord */
+			horaire_de_passage_a_l_heure_d_ete_pour_l_amerique_du_nord_sous_forme_de_chaine_de_caracteres = calcul_et_renvoie_de_l_heure_et_de_la_date_en_fonction_du_timestamp_passe_en_parametre(horaire_de_passage_a_l_heure_d_ete_pour_l_amerique_du_nord_sous_forme_de_timestamp);
+			horaire_de_passage_a_l_heure_d_hiver_pour_l_amerique_du_nord_sous_forme_de_chaine_de_caracteres = calcul_et_renvoie_de_l_heure_et_de_la_date_en_fonction_du_timestamp_passe_en_parametre(horaire_de_passage_a_l_heure_d_hiver_pour_l_amerique_du_nord_sous_forme_de_timestamp);
 
 			//Affichage de tous les changements d'heure heure d'été <=> heure d'hiver pour l'année annee_voulue
 
@@ -79,6 +91,13 @@ int main(int argc, char* argv[])
 			/* Europe continentale */
 			printf("Passage à l'heure d'été en %d pour l'Europe continentale: %s\n", annee_voulue, horaire_de_passage_a_l_heure_d_ete_pour_l_europe_continentale_sous_forme_de_chaine_de_caracteres);
 			printf("Passage à l'heure d'hiver en %d pour l'Europe continentale: %s\n", annee_voulue, horaire_de_passage_a_l_heure_d_hiver_pour_l_europe_continentale_sous_forme_de_chaine_de_caracteres);
+
+			//
+			printf("\n");
+
+			/* Amérique du Nord */
+			printf("Passage à l'heure d'été en %d pour l'Amérique du Nord: %s\n", annee_voulue, horaire_de_passage_a_l_heure_d_ete_pour_l_amerique_du_nord_sous_forme_de_chaine_de_caracteres);
+			printf("Passage à l'heure d'hiver en %d pour l'Amérique du Nord: %s\n", annee_voulue, horaire_de_passage_a_l_heure_d_hiver_pour_l_amerique_du_nord_sous_forme_de_chaine_de_caracteres);
 
 		}
 		//Sinon...
