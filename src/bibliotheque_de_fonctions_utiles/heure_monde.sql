75e7404a7e7d61277f828cb1bfb2632ff37108c0
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2019-09-04 16:30:53 +0200

    Changement du stockage de fichier CSV à SQLITE

---
+++ src/bibliotheque_de_fonctions_utiles/heure_monde.sql
@@ -0,0 +1,7 @@
+-- Création de la table table_des_decalages_horaires --
+CREATE TABLE IF NOT EXISTS table_des_decalages_horaires(
+	nom_de_la_timezone String,
+	decalage_par_rapport_a_UTC int
+)
+
+-- Insertion des décalages horaires relatives à chaque timezone -- 
